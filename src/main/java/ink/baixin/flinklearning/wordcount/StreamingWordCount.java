4a1efddf9fedec38dc7dd28c089390f8e6580400
baixin <baixin@bilibili.com>
2022-04-18 10:09:00 +0800

    Add StreamingWordCount

---
+++ src/main/java/ink/baixin/flinklearning/wordcount/StreamingWordCount.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ink.baixin.flinklearning.wordcount;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.datagen.DataGeneratorSource;
+import org.apache.flink.streaming.api.functions.source.datagen.RandomGenerator;
+import org.apache.flink.util.Collector;
+
+/**
+ * Implements a streaming version of the "WordCount" program.
+ */
+public class StreamingWordCount {
+
+    public static void main(String[] args) throws Exception {
+
+        // get the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // source
+        DataStream<String> stream = env
+                .addSource(new DataGeneratorSource<>(RandomGenerator.stringGenerator(10), 10000, null))
+                .setParallelism(1)
+                .returns(Types.STRING);
+
+        // parse the data, group it, and aggregate the counts
+        DataStream<WordWithCount> counts = stream
+                .flatMap(new FlatMapFunction<String, WordWithCount>() {
+                    @Override
+                    public void flatMap(
+                            String value, Collector<WordWithCount> out) {
+                        for (String word : value.split("\\s")) {
+                            out.collect(new WordWithCount(word, 1L));
+                        }
+                    }
+                })
+                .setParallelism(2)
+                .keyBy(value -> value.word)
+                .sum("count")
+                .setParallelism(2);
+
+        // print the results with a single thread, rather than in parallel
+        counts.print().setParallelism(2);
+
+        env.execute("Streaming WordCount");
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Data type for words with count.
+     */
+    public static class WordWithCount {
+
+        public String word;
+        public long count;
+
+        public WordWithCount() {
+
+        }
+
+        public WordWithCount(String word, long count) {
+            this.word = word;
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return word + " : " + count;
+        }
+    }
+}
